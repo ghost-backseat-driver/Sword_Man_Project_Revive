0d765a703f2bcad1382620bd48a4a200b04a509b
ghost-backseat-driver <ysl2323@naver.com>
2025-10-27 10:04:44 +0900

    level1 - 옵션패널 활성화 시 메뉴패널 비활성화, 옵션패널 비활성화시, 메뉴패널 활성화 기능 추가 세팅UI 스크립트 수정함

--- Assets/01.Scenes/Title_Scene/Used_by_Title/Scripts_Title/Setting_UI.cs
+++ Assets/01.Scenes/Title_Scene/Used_by_Title/Scripts_Title/Setting_UI.cs
@@ -7,6 +7,8 @@ public class Setting_UI : MonoBehaviour
 {
     [Header("세팅 패널")]
     [SerializeField] private GameObject settingPanel; // 세팅 패널
+    [SerializeField] private GameObject menuPanel; // 메뉴 패널
+
 
     [Header("세팅 버튼 open/close")]
     [SerializeField] private Button openButton;  // 타이틀 화면에서 세팅 버튼
@@ -32,12 +34,15 @@ public class Setting_UI : MonoBehaviour
     private void OpenSettings()
     {
         SoundManager.Instance.PlayEffect("Button_Move_SFX");//사운드 추가
-        settingPanel.SetActive(true); //열리면 활성화
+        settingPanel.SetActive(true); //열리면 세팅패널 활성화
+        menuPanel.SetActive(false); //메뉴패널 비활성화
+
     }
 
     private void CloseSettings()
     {
         SoundManager.Instance.PlayEffect("Cancel_SFX");
-        settingPanel.SetActive(false); //닫으면 다시 비활성화
+        settingPanel.SetActive(false); //닫으면 세팅패널 비활성화
+        menuPanel.SetActive(true); //닫으면 메뉴패널 활성화
     }
 }
